22593004c30bd7fb98f131cb6680f44b25319bf6
Abzal Serekov <abzal.serekov@gmail.com>
2015-06-25 15:12:39 -0500

    done

--- parser/parser.py
+++ parser/parser.py
@@ -3,29 +3,44 @@
 
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('cities/kyzylorda.xml')
-root = tree.getroot()
+from os import listdir
+from os.path import isfile, join
 
-geo_data = []
-street_data = []
+mypath = 'cities/'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
-for i in range(len(root)):
-	for j in range(len(root[i])):
-		if root[i][j].text is not None:
-			if 'geoposition' in unicode(root[i][j]):
-				geo_data.append(unicode(root[i][j].text).lstrip())
-				break
-			if 'street' in unicode(root[i][j]) and not u'НЕИЗВЕСТНАЯ' in unicode(root[i][j].text):
-				street_data.append(unicode(root[i][j].text).lstrip())
+print onlyfiles
 
-f = open('streets/kyzylorda.txt', 'w')
-for street in street_data:
-	f.write(street.encode('utf-8'))
-	f.write('\n')
-f.close()
+for city in onlyfiles:
 
-f = open('geopositions/kyzylorda.txt', 'w')
-for geoposition in geo_data:
-	f.write(geoposition.encode('utf-8'))
-	f.write('\n')
-f.close()
+	print city
+
+	city_name = city.split('.')[0]
+
+	print city_name
+
+	tree = ET.parse('cities/' + city)
+	root = tree.getroot()
+	geo_data = []
+	street_data = []
+
+	for i in range(len(root)):
+		for j in range(len(root[i])):
+			if root[i][j].text is not None:
+				if 'geoposition' in unicode(root[i][j]):
+					geo_data.append(unicode(root[i][j].text).lstrip())
+					break
+				if 'street' in unicode(root[i][j]) and not u'НЕИЗВЕСТНАЯ' in unicode(root[i][j].text):
+					street_data.append(unicode(root[i][j].text).lstrip())
+
+	f = open('streets/'+city_name+'.txt', 'w')
+	for street in street_data:
+		f.write(street.encode('utf-8'))
+		f.write('\n')
+	f.close()
+
+	f = open('geopositions/'+city_name+'.txt', 'w')
+	for geoposition in geo_data:
+		f.write(geoposition.encode('utf-8'))
+		f.write('\n')
+	f.close()
