189381fbcd6966030736d00bee357a00a879a1f7
Abzal Serekov <abzal.serekov@gmail.com>
2015-06-25 18:55:23 -0500

    parser for JS (visualization)

---
+++ parser/parser_js.py
@@ -0,0 +1,49 @@
+
+# coding=utf-8
+
+import xml.etree.ElementTree as ET
+
+from os import listdir
+from os.path import isfile, join
+
+# mypath = 'cities/'
+# onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+
+# print onlyfiles
+
+onlyfiles = ['kyzylorda.xml']
+
+for city in onlyfiles:
+
+	print city
+
+	city_name = city.split('.')[0]
+
+	print city_name
+
+	tree = ET.parse('cities/' + city)
+	root = tree.getroot()
+	geo_data = []
+	street_data = []
+
+	for i in range(len(root)):
+		for j in range(len(root[i])):
+			if root[i][j].text is not None:
+				if 'geoposition' in unicode(root[i][j]):
+					geo_data.append(unicode(root[i][j].text).lstrip())
+					break
+				if 'street' in unicode(root[i][j]) and not u'НЕИЗВЕСТНАЯ' in unicode(root[i][j].text):
+					street_data.append(unicode(root[i][j].text).lstrip())
+
+	f = open('streets/'+city_name+'.txt', 'w')
+	for street in street_data:
+		f.write(street.encode('utf-8'))
+		f.write('\n')
+	f.close()
+
+	f = open('geopositions/'+city_name+'.txt', 'w')
+	for geoposition in geo_data:
+		f.write(u'new google.maps.LatLng(%s),'%(geoposition.encode('utf-8')))
+		f.write('\n')
+
+	f.close()
