43e9a79d5c1c74cd8b3a72b152edab6c35d6ca6e
Abzal Serekov <abzal.serekov@gmail.com>
2015-06-25 14:31:41 -0500

    parser for kyzylorda is done

--- parser/parser.py
+++ parser/parser.py
@@ -2,3 +2,24 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('cities/kyzylorda.xml')
 root = tree.getroot()
+
+geo_data = []
+street_data = []
+
+for i in range(len(root)):
+	for j in range(len(root[i])):
+		if root[i][j].text is not None:
+			if 'geoposition' in unicode(root[i][j]):
+				geo_data.append(unicode(root[i][j].text))
+				break
+			if 'street' in unicode(root[i][j]):
+				street_data.append(unicode(root[i][j].text))
+
+f = open('street_data.txt', 'w')
+for street in street_data:
+	f.write(street.encode('utf-8'))
+
+print len(geo_data)
+print len(street_data)
+print len(root)
+
