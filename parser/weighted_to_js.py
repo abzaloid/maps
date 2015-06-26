079d31bf60dc0b447e8311890e4f2965f8972658
Abzal Serekov <abzal.serekov@gmail.com>
2015-06-26 03:16:21 -0500

    random 1000 points are selected

--- parser/weighted_to_js.py
+++ parser/weighted_to_js.py
@@ -1,13 +1,17 @@
 import csv
+import random
 
 res = []
 
 with open('weighted_geopositions/kyzylorda_weighted.txt', 'rb') as csvfile:
     georeader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in georeader:
-    	# res.append('{location: new google.maps.LatLng(%s, %s), weight: %s},'%(row[0],row[1],row[2]))
-    	res.append('%s,%s,%s'%(row[0],row[1],row[2]))
+    	res.append('{location: new google.maps.LatLng(%s, %s), weight: %s},'%(row[0],row[1],min(float(row[2]), 10.0)))
+    	# res.append('%s,%s,%s'%(row[0],row[1],min(float(row[2]), 50)))
 f = open('weighted_geopositions_js/kyzylorda_js.txt', 'w')
+
+if len(res) > 1000:
+	res = random.sample(res, 1000)
 
 for r in res:
 	f.write(r+'\n')
