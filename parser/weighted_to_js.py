4f3f898dd2ccd7c3d27c8d7022f73959a3da2f5b
Abzal Serekov <abzal.serekov@gmail.com>
2015-06-26 16:31:39 -0500

    done

--- parser/weighted_to_js.py
+++ parser/weighted_to_js.py
@@ -11,7 +11,7 @@ for city in files:
 	with open('weighted_geopositions/%s_weighted.txt' % city_name, 'rb') as csvfile:
 	    georeader = csv.reader(csvfile, delimiter=',', quotechar='|')
 	    for row in georeader:
-	    	res.append('{location: new google.maps.LatLng(%s, %s), weight: %s},'%(row[0],row[1],min(float(row[2]), 10000.0)))
+	    	res.append('{location: new google.maps.LatLng(%s, %s), weight: %s},'%(row[0],row[1],min(float(row[2]), 65.0)))
 	    	# res.append('%s,%s,%s'%(row[0],row[1],min(float(row[2]), 50)))
 	f = open('weighted_geopositions_js/%s_js.txt' % city_name, 'w')
 
